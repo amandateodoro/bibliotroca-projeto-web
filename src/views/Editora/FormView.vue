511c6699833cfb812f08e7b88659cca460811902
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2025-06-11 17:50:42 -0400

    update forms
    Co-Authored-By: Amanda Teodoro <85532804+amandateodoro@users.noreply.github.com>

--- src/views/Editora/FormView.vue
+++ src/views/Editora/FormView.vue
@@ -8,43 +8,71 @@
         </div>
       </div>
       <div class="container px-0 pb-2">
-        <form>
+        <form @submit.prevent="salvar">
           <div class="row">
             <div class="col-5">
               <label for="txtName" class="form-label">Nome <a style="color: red;">*</a></label>
-              <input type="text" class="form-control p-2" id="txtName" placeholder=" Nome da Editora">
+              <input type="text" class="form-control p-2" id="txtName" placeholder=" Nome da Editora" v-model="formDados.nome">
+              <div class="text-danger"  v-if="v$.formDados.nome.$errors.length">
+                  <p class="fs-6" v-for="error of v$.formDados.nome.$errors" :key="error.$uuid">{{ error.$message }}</p>
+                </div>
             </div>
           </div>
           <div class="row mt-2">
             <div class="col-5">
               <label for="txtUrl" class="form-label">URL</label>
-              <input type="url" class="form-control p-2" id="txtUrl" placeholder=" URL do site da Editora">
+              <input type="url" class="form-control p-2" id="txtUrl" placeholder=" URL do site da Editora" v-model="formDados.url">
             </div>
           </div>
           <div class="row mt-2">
             <div class="col-5">
-              <label for="txtContact" class="form-label">Contato</label>
-              <input type="email" class="form-control p-2" id="txtContact" placeholder=" E-mail para contato">
+              <label for="txtContact" class="form-label">Email Contato</label>
+              <input type="email" class="form-control p-2" id="txtContact" placeholder=" E-mail para contato" v-model="formDados.email">
+              <div class="text-danger"  v-if="v$.formDados.email.$errors.length">
+                  <p class="fs-6" v-for="error of v$.formDados.email.$errors" :key="error.$uuid">{{ error.$message }}</p>
+                </div>
             </div>
           </div>
-          <div class="d-flex justify-content-start">
-              <a href="/editoras" type="button" class="btn btn-secondary mt-4 px-9">Cadastrar</a>
-            </div>
+          <div class="d-flex justify-content-start gap-3">
+            <RouterLink to="/editoras" type="button" class="btn btn-secondary mt-4 px-9">Cadastrar</RouterLink>
+            <button type="submit" class="btn btn-secondary mt-4 px-9">Cadastrar</button>
+          </div>
         </form>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
+import useVuelidate from "@vuelidate/core";
+import { email, helpers, minLength, required } from "@vuelidate/validators";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: 'FormView',
 
+  setup() {
+    return {
+      v$: useVuelidate()
+    }
+  },
+
   data() {
     return {
-      listaEditoras: [] as Array<{ nome: string; url: string; email: string }>,
-      }
+      formDados: {
+        nome: '',
+        url: '',
+        email: ''
+      },
+    }
+  },
+
+  validations() {
+    return {
+      formDados: {
+        nome: { required: helpers.withMessage('O nome é Obrigatório', required), minLength: helpers.withMessage('Nome completo da Editora !', minLength(4)) },
+        email: { required: helpers.withMessage('O email é obrigatório', required), email: helpers.withMessage('O email é inválido!', email) }
+      },
+    }
   },
 
   mounted() {
@@ -52,7 +80,16 @@ export default defineComponent({
   },
 
   methods: {
+    async salvar() {
+      const result = await this.v$.$validate()
 
+      if (!result) {
+        return
+      }
+
+      console.log('Dados dO Formulário', this.formDados);
+
+    }
   }
 });
 </script>
--- src/views/Estado/FormView.vue
+++ src/views/Estado/FormView.vue
@@ -8,37 +8,66 @@
         </div>
       </div>
       <div class="container px-0 pb-2">
-        <form>
+        <form @submit.prevent="salvar">
           <div class="row">
             <div class="col-5">
               <label for="txtName" class="form-label">Nome <a style="color: red;">*</a></label>
-              <input type="text" class="form-control p-2" id="txtName" placeholder=" Nome da Cidade">
+              <input type="text" class="form-control p-2" id="txtName" placeholder=" Nome da Cidade"
+                v-model="formDados.nome">
+              <div class="text-danger" v-if="v$.formDados.nome.$errors.length">
+                <p class="fs-6" v-for="error of v$.formDados.nome.$errors" :key="error.$uuid">{{ error.$message }}</p>
+              </div>
             </div>
           </div>
           <div class="row mt-2">
             <div class="col-5">
-                <label for="txtSigla" class="form-label">UF <a style="color: red;">*</a></label>
-                <input type="text" class="form-control p-2" id="txtSigla" placeholder=" Sigla do Estado">
+              <label for="txtSigla" class="form-label">UF <a style="color: red;">*</a></label>
+              <input type="text" class="form-control p-2" id="txtSigla" placeholder=" Sigla do Estado" maxlength="2"
+                v-model="formDados.uf">
+              <div class="text-danger" v-if="v$.formDados.uf.$errors.length">
+                <p class="fs-6" v-for="error of v$.formDados.uf.$errors" :key="error.$uuid">{{ error.$message }}</p>
               </div>
-          </div>
-          <div class="d-flex justify-content-start">
-              <a href="/cidades" type="button" class="btn btn-secondary mt-4 px-9">Cadastrar</a>
             </div>
+          </div>
+          <div class="d-flex justify-content-start gap-3">
+            <RouterLink to="/cidades" type="button" class="btn btn-secondary mt-4 px-9">Voltar</RouterLink>
+            <button type="submit" class="btn btn-secondary mt-4 px-9">Cadastrar</button>
+          </div>
         </form>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
+import useVuelidate from "@vuelidate/core";
+import { helpers, maxLength, minLength, required } from "@vuelidate/validators";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: 'FormView',
 
+  setup() {
+    return {
+      v$: useVuelidate()
+    }
+  },
+
   data() {
     return {
-      listaEstados: [] as Array<{ uf: string; nome: string }>,
-      }
+      formDados: {
+        uf: '',
+        nome: ''
+      },
+    }
+  },
+
+  validations() {
+    return {
+      formDados: {
+        uf: { required: helpers.withMessage('A sigla (UF) é Obrigatório', required), maxLength: helpers.withMessage('UF precisa conter no máximo 2 letras!', maxLength(2)) },
+        nome: { required: helpers.withMessage('O nome é Obrigatório', required), minLength: helpers.withMessage('Nome precisa ser um Estado Válido!', minLength(4)) }
+      },
+    }
   },
 
   mounted() {
@@ -46,7 +75,15 @@ export default defineComponent({
   },
 
   methods: {
+    async salvar() {
+      const result = await this.v$.$validate()
+      if (!result) {
+        return
+      }
 
+      console.log('Dados do Formulário', this.formDados);
+
+    }
   }
 });
 </script>
